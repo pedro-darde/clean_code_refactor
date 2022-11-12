61cb31bd398121e2a99e228a92b0541b8dcd66ed
pedro.darde <pedro.darde@multi24h.com.br>
2022-11-12 11:31:29 -0300

    changes

--- src/infra/repository/database/OrderRepositoryDatabase.ts
+++ src/infra/repository/database/OrderRepositoryDatabase.ts
@@ -8,10 +8,10 @@ import { OrderRepository } from "../../../domain/repository/OrderRepository";
 import Connection from "../../database/Connection";
 
 export class OrderRepositoryDatabase implements OrderRepository {
-  constructor(readonly connection: Connection) { }
+  constructor(readonly connection: Connection) {}
 
   async save(order: Order): Promise<void> {
-    throw new Error(`Not implemented`)
+    throw new Error(`Not implemented`);
   }
 
   async getByCpf(cpf: string): Promise<Order[]> {
@@ -27,15 +27,27 @@ export class OrderRepositoryDatabase implements OrderRepository {
         orderData.issue_date,
         orderData.sequence
       );
-      const [orderItemSData] = await this.connection.query("SELECT * FROM public.order_item WHERE id_order = $1", [orderData.id_order])
+      const [orderItemSData] = await this.connection.query(
+        "SELECT * FROM public.order_item WHERE id_order = $1",
+        [orderData.id_order]
+      );
       for (const orderItemData of orderItemSData) {
-        order.items.push(new OrderItem(orderItemData.id_item, parseFloat(orderItemData.price), orderItemData.quantity))
+        order.items.push(
+          new OrderItem(
+            orderItemData.id_item,
+            parseFloat(orderItemData.price),
+            orderItemData.quantity
+          )
+        );
       }
       if (orderData.coupon_code) {
-        order.coupon = new OrderCoupon(orderData.coupon_code, orderData.coupon_percentage)
+        order.coupon = new OrderCoupon(
+          orderData.coupon_code,
+          orderData.coupon_percentage
+        );
       }
-      order.freigth = parseFloat(orderData.freight)
-      orders.push(order)
+      order.freigth = parseFloat(orderData.freight);
+      orders.push(order);
     }
 
     return orders;
@@ -47,12 +59,13 @@ export class OrderRepositoryDatabase implements OrderRepository {
       []
     );
     return row.count;
-
   }
 
   async clear(): Promise<void> {
-    await this.connection.query("TRUNCATE public.order_item CASCADE", []);
-    await this.connection.query("TRUNCATE public.order CASCADE", []);
-    await this.connection.query("TRUNCATE public.coupon CASCADE", [])
+    await Promise.all([
+      this.connection.query("TRUNCATE public.order_item CASCADE", []),
+      this.connection.query("TRUNCATE public.order CASCADE", []),
+      this.connection.query("TRUNCATE public.coupon CASCADE", []),
+    ]);
   }
 }
